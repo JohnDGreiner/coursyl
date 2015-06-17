3b1cae3e0d3625d9ee268385b3d905c1dcecb355
JohnDGreiner <john.d.greiner@gmail.com>
2015-06-17 07:50:26 -0400

    grade_threshold row manipulation using js.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,37 @@
 //= require jquery_ujs
 //= require d3
 //= require_tree .
+
+function hideLastRow() {
+  var associations = document.getElementsByClassName("association container");
+  var lastAssociation = associations[associations.length-1];
+  lastAssociation.style.display = "none";
+}
+
+window.onload = hideLastRow
+
+function disableUpdateButton() {
+  var elements = document.getElementsByClassName("btn");
+  var last = elements[elements.length-1];
+  last.disabled=true;
+}
+
+function showLastRow() {
+  var associations = document.getElementsByClassName("association container");
+  var lastAssociation = associations[associations.length-1];
+  lastAssociation.style.display = "block";
+}
+
+function hideDeleteRow(position) {
+  var associations = document.getElementsByClassName("association container");
+  var associationRow = associations[position];
+  associationRow.style.display = "none";
+  markDelete(position);
+
+}
+
+function markDelete(position) {
+  var associations = document.getElementsByClassName("destroy");
+  var associationRow = associations[position];
+  associationRow.checked = true;
+}
